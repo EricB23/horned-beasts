00512d8ca041fb8f7dfdff40e9d9225484307624
Eric Baxter <lamarbaxter14@gmail.com>
2023-08-19 11:35:55 -0500

    debugged gallery jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component } from 'react';
 import HornedBeast from './HornedBeast';
 
 export default class Gallery extends Component {
@@ -8,21 +8,24 @@ export default class Gallery extends Component {
       name: 'Another Beast',
       imageUrl: 'imageUrl',
       description: 'description',
-    }
+    };
   }
   render() {
-  let array = [1,2]
-  let [test, subject] = array
-  let {name, imageUrl, description} = this.state
-  let url1 = 'laura-college-K_Na5gCmh38-unsplash.jpg'
-  let url2 = 'nick-fewings-c4WBrQGR5q0-unsplash.jpg'
-  return {
-    <>
-
-    <div> Gallery </div>
-    <HornedBeast description= 'Deer in Forest' title= 'Deer' image={url1} />
-    <HornedBeast description= 'Buck in Wilderness' title= 'Buck' image={url2}/>
-
-  };
+    let array = [1, 2];
+    let [test, subject] = array;
+    let { name, imageUrl, description } = this.state;
+    let url1 = 'laura-college-K_Na5gCmh38-unsplash.jpg';
+    let url2 = 'nick-fewings-c4WBrQGR5q0-unsplash.jpg';
+    return (
+      <>
+        <div> Gallery </div>
+        <HornedBeast description="Deer in Forest" title="Deer" image={url1} />
+        <HornedBeast
+          description="Buck in Wilderness"
+          title="Buck"
+          image={url2}
+        />
+      </>
+    );
   }
 }
